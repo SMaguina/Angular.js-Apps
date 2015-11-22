d7a984ee9b5335aad6b9792cd41fda34fb2f37bc
Sylvia Maguiña <smag23@gmail.com>
2015-11-21 21:57:07 -0800

    Import controller.js

---
+++ Contact Directory App/js/controllers.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sylvia on 11/6/15.
+ */
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+    $http.get('js/data.json').success(function(data) {
+        $scope.artists = data;
+        $scope.artistOrder = 'name';
+    });
+}]);
