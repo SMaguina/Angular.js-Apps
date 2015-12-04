3eb3746918a1c191bee0e8138ad61ee75f2545cd
SMaguina <smag23@gmail.com>
2015-12-03 21:22:09 -0800

    Connect AJAX GET request
    -Connect to the Users API
    -Create success and error functions

--- Custom Scripts/app.js
+++ Custom Scripts/app.js
@@ -1,20 +1,39 @@
 var app = angular.module("myApp", []);
 
-app.controller("testCtrl", function($scope) {
+app.controller("testCtrl", function($scope, $http) {
 	$scope.userText = "My default text!";
 
-	$scope.users = [
-	{
-		firstname: "Bob",
-		lastname: "Jones",
-		age: 24,
-		username: "bjones"
-	},
-	{
-		firstname: "Joan",
-		lastname: "Randell",
-		age: 35,
-		username: "jrandell"
-	}
-	];
+	//HTTP Request Types
+
+	//GET -> Read
+	//POST -> Create
+	//PUT -> Update
+	//DELETE -> Delete
+
+	$http.get("http://daretodiscover.herokuapp.com/users")
+		.success(function(users) {
+			console.log(users);
+				//Step 1: Loop through users array
+				//Step 2: Take specific user and input their information into HTML template
+				//Step 3: $.append() to add new HTML to DOM
+				$scope.users = users;
+		})
+		.error(function() {
+			alert("Error getting data");
+		});
+
+	/*$scope.users = [
+		{
+			firstname: "Bob",
+			lastname: "Jones",
+			age: 24,
+			username: "bjones"
+		},
+		{
+			firstname: "Joan",
+			lastname: "Randell",
+			age: 35,
+			username: "jrandell"
+		}
+	];*/
 });
